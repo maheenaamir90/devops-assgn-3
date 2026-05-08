8543ad4393aaf4f6548771ee3b25c6f05a191305
Ubuntu <ubuntu@ip-172-31-11-238.ec2.internal>
2026-05-08 10:12:16 +0000

    Initial Commit

---
+++ tests/test_app.py
@@ -0,0 +1,25 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
+
+driver.get("http://YOUR_PUBLIC_IP:5000")
+
+# Test Case 1
+assert "DevOps Assignment Running Successfully" in driver.page_source
+
+# Test Case 2
+title = driver.title
+assert title == ""
+
+print("All Selenium Tests Passed")
+
+driver.quit()
